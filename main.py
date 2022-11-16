f375abe4e680cc0eb0750e51bc93b26aaacc4835
Jean28518 <jf_v@gmx.de>
2022-11-16 12:24:04 +0100

    Add basic functionality

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+class Feedback(BaseModel):
+    message: str
+    entries: list
+    searchTerm: str
+    environment : dict
+
+
+app = FastAPI()
+
+@app.post("/submit/")
+async def submit(feedback: Feedback):
+    now = datetime.now()
+    file_name = now.strftime("%Y-%m-%d_%H:%M:%S") + ".json"
+    f = open("./feedback/" + file_name, "a")
+    f.write(feedback.json())
+    f.close()
+
+    
+    return {"message": "success"}
