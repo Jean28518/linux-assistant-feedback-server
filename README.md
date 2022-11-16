24c41d541d79a8e95cc0f90ece096eb04bacec55
Jean28518 <jf_v@gmx.de>
2022-11-16 17:18:02 +0100

    Pretty json and run manual

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # linux-assistant-feedback-server
 Feedback Server written in FastAPI
+
+## How to run:
+
+```bash
+bash run.sh
+```
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def submit(feedback: Feedback):
     now = datetime.now()
     file_name = now.strftime("%Y-%m-%d_%H:%M:%S") + ".json"
     f = open("./feedback/" + file_name, "a")
-    f.write(feedback.json())
+    f.write(feedback.json(indent=4))
     f.close()
 
     
